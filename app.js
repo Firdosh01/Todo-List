f3f5826fed7105ba139ae1195c9bbb58269f6278
Firdosh01 <firdosh7392@gmail.com>
2023-03-14 07:40:33 -0500

    delete task logic

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.querySelector('#push').onclick = function () {
     if (document.querySelector('#newtask input').value.length == 0) {
         alert("Please Enter a Task")
     } else {
-        document.querySelector('#tasks').innerHTML 
+        document.querySelector('#task-list').innerHTML 
         += `<div class="task">
                  <span id="taskname">
                       ${document.querySelector('#newtask input').value}
@@ -11,5 +11,19 @@ document.querySelector('#push').onclick = function () {
                  <ion-icon name="trash-outline"></ion-icon>
                  </button>
             </div>` ;
+
+            var current_task = document.querySelectorAll(".delete");
+            for(var i=0; i<current_task.length; i++) {
+                current_task[i].onclick = function() {
+                    this.parentNode.remove();
+                }
+            }
+            var tasks = document.querySelectorAll(".task");
+            for(var i=0; i<tasks.length; i++){
+                tasks[i].onclick = function() {
+                    this.classList.toggle('completed')
+                }
+            }
+            document.querySelector("#newtask input").value = "";
     }
 }
